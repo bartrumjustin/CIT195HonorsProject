de700c9c531eb78bd0600e727032039ef9b46854
bartrumjustin <bartrumjustin@gmail.com>
2026-04-21 22:59:25 -0400

    Add project files.

---
+++ Models/HardDrive.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIT195HonorsProject.Models
+{
+    public class HardDrive
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Manufacturer")]
+        public string Manufacturer { get; set; } = string.Empty;
+
+        [Range(1, 100000)]
+        [Display(Name = "Capacity (GB)")]
+        public int CapacityGB { get; set; }
+
+        [Display(Name = "Is SSD")]
+        public bool IsSolidState { get; set; }
+
+        [Display(Name = "Assigned Cluster")]
+        public int NodeClusterId { get; set; }
+
+        // Nav prop
+        public NodeCluster? NodeCluster { get; set; }
+    }
+}
---
+++ Models/NodeCluster.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CIT195HonorsProject.Models
+{
+    public class NodeCluster
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        [Display(Name = "Cluster Location")]
+        public string ClusterLocation { get; set; } = string.Empty;
+
+        // Nav prop
+        public ICollection<HardDrive>? HardDrives { get; set; }
+    }
+}
